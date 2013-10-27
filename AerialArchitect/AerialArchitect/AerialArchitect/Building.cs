396cd3b655f467d37dbd32bba5c4494c807e4dfb
noogai03 <0xzeroex@gmail.com>
2013-10-27 08:21:25 +0000

    Initial commit
    Some stuff added

---
+++ AerialArchitect/AerialArchitect/AerialArchitect/Building.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NLua;
+
+namespace AerialArchitect
+{
+    class Building
+    {
+        public float Floating;
+        private Lua lua;
+    }
+}
---
+++ AerialArchitect/AerialArchitect/AerialArchitect/Resources.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AerialArchitect
+{
+    class Resources
+    {
+        class Minerals
+        {
+
+        }
+
+        class Labour
+        {
+
+        }
+
+        class Dollars
+        {
+
+        }
+    }
+}
